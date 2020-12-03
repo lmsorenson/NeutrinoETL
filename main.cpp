497abb80e075f61ea4d52fdb506593cf66b1b959
Lucas Sorenson <lsorenson20@gmail.com>
2020-12-03 08:26:51 -0600

    fix error

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 #include <src/models/neutrinotrack.h>
 
-void on_new_track(NeutrinoTrack * track, QString data, QString path);
+void on_new_track(NeutrinoTrack **track, QString data, QString path);
 void on_new_point(NeutrinoTrack * track, QString data);
 
 int main(int argc, char *argv[])
@@ -28,13 +28,12 @@ int main(int argc, char *argv[])
     NeutrinoTrack *current_track = nullptr;
     for(int i = 0; i < array.size(); ++i)
     {
-        qDebug() << i;
         auto id_prefix = QString("track ID");
         auto prefix = QString("Point no");
-        auto record = QString(array[i]);
+        auto record = QString(array[i]).trimmed();
 
         if (record.startsWith(id_prefix))
-            on_new_track(current_track, QStringRef(&record, id_prefix.size(), record.size() - id_prefix.size()).toString(), "");
+            on_new_track(&current_track, QStringRef(&record, id_prefix.size(), record.size() - id_prefix.size()).toString(), "");
 
         if (record.startsWith(prefix))
             on_new_point(current_track, QStringRef(&record, prefix.size(), record.size() - prefix.size()).toString());
@@ -48,26 +47,26 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void on_new_track(NeutrinoTrack * track, QString data, QString path)
+void on_new_track(NeutrinoTrack **track, QString data, QString path)
 {
-    qDebug() << data;
-
-    if (track)
+    if (track != nullptr)
     {
-        track->to_disk(path);
-        delete track;
-        track = nullptr;
+        (*track)->to_disk(path);
+        delete *track;
+        *track = nullptr;
     }
 
     bool ok;
     auto id = data.trimmed().toInt(&ok);
 
     if (ok)
-        track = new NeutrinoTrack(id);
+        *track = new NeutrinoTrack(id);
 }
 
-void on_new_point(NeutrinoTrack * track, QString data)
+void on_new_point(NeutrinoTrack *track, QString data)
 {
+    if (track == nullptr) return;
+
     bool ok;
     QMap<QString, float> map = QMap<QString, float>();
     auto parts = data.trimmed().split(" ");
@@ -75,7 +74,6 @@ void on_new_point(NeutrinoTrack * track, QString data)
     float point_no = parts.takeFirst().toFloat(&ok);
     if (!ok) return;
 
-    qDebug() << "Label: " << "Point no" << " Value: " << point_no;
     map.insert("Point no", point_no);
 
     for(int i = 0; i+1 < parts.size(); i += 2)
@@ -84,10 +82,8 @@ void on_new_point(NeutrinoTrack * track, QString data)
         float value = parts[i+1].toFloat(&ok);
         if (!ok) return;
 
-        qDebug() << "Label: " << label << " Value: " << value;
         map.insert(label, value);
     }
 
-    qDebug() << "new point";
     track->add_point(new NeutrinoPoint(map));
 }
